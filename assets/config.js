bf7847fabc34ebcdcb7805871718222a50b47c8b
Lewis De La Cruz <62686249+ulewis@users.noreply.github.com>
2026-09-17 15:44:49 -0500

    Conectar backend y WhatsApp de la tienda

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,7 @@
 window.STORE_CONFIG = {
   STORE_NAME: 'Store Flowers',
-  API_URL: '',
-  PUBLIC_WHATSAPP: '',
+  API_URL: 'https://script.google.com/macros/s/AKfycbydlj7_SiUiVNgcIHZIFSHdThgm2lxWKZJztTbuE4M3qq3cnXvVEj7namoNklUPRTWw/exec',
+  PUBLIC_WHATSAPP: '51956892798',
   RESERVATION_MINUTES: 30,
   DEFAULT_CITY: 'Piura',
   DEFAULT_REGION: 'Piura',
